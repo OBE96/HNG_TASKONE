50cac249f25589c4e3db75c0040059453bc94cf1
OBE96 <brightelo1@gmail.com>
2025-02-03 18:09:44 +0100

    initialcommit

--- HNG_TASK1/Program.cs
+++ HNG_TASK1/Program.cs
@@ -1,8 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
+
+// Add services to the container.
+builder.Services.AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        // Customize the response for invalid model states
+        options.InvalidModelStateResponseFactory = context =>
+        {
+            var result = new BadRequestObjectResult(new { error = true });
+            result.ContentTypes.Add("application/json");
+            return result;
+        };
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

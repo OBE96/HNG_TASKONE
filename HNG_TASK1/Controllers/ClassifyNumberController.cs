36130b6c7a6b121ff01f2d91519557e2f4ee22eb
OBE96 <brightelo1@gmail.com>
2025-02-03 18:04:06 +0100

    modify spees of api

--- HNG_TASK1/Controllers/ClassifyNumberController.cs
+++ HNG_TASK1/Controllers/ClassifyNumberController.cs
@@ -1,5 +1,4 @@
 ﻿using HNG_TASK1.Model;
-using HNG_TASK1.Service;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,10 +8,73 @@ namespace HNG_TASK1.Controllers
     [ApiController]
     public class ClassifyNumberController : ControllerBase
     {
-        private readonly NumberService _numberService;
-        public ClassifyNumberController()
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        // Helper methods for number classification
+        private bool IsPrime(int number)
         {
-            _numberService = new NumberService();
+            if (number < 2)
+                return false;
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        private bool IsPerfect(int number)
+        {
+            if (number < 2)
+                return false;
+            int sum = Enumerable.Range(1, number - 1).Where(i => number % i == 0).Sum();
+            return sum == number;
+        }
+
+        private bool IsArmstrong(int number)
+        {
+            string digits = number.ToString();
+            int length = digits.Length;
+            int sum = digits.Sum(d => (int)Math.Pow(int.Parse(d.ToString()), length));
+            return sum == number;
+        }
+
+        private int DigitSum(int number)
+        {
+            return number.ToString().Sum(c => int.Parse(c.ToString()));
+        }
+
+        private async Task<string> GetFunFact(int number)
+        {
+            try
+            {
+                string url = $"http://numbersapi.com/{number}/math?json=true";
+                HttpResponseMessage response = await _httpClient.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var factJson = await response.Content.ReadAsStringAsync();
+                    using (var doc = System.Text.Json.JsonDocument.Parse(factJson))
+                    {
+                        if (doc.RootElement.TryGetProperty("text", out var factElement))
+                        {
+                            return factElement.GetString();
+                        }
+                        else
+                        {
+                            return "No fun fact available.";
+                        }
+                    }
+                }
+                else
+                {
+                    return "No fun fact available.";
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"An error occurred while fetching the fact: {ex.Message}";
+            }
         }
 
         [HttpGet]
@@ -35,14 +97,14 @@ namespace HNG_TASK1.Controllers
             var response = new NumberPropertiesResponse
             {
                 number = parsedNumber,
-                is_Prime = _numberService.IsPrime(parsedNumber),
-                is_Perfect = _numberService.IsPerfect(parsedNumber),
-                digit_sum= _numberService.DigitSum(parsedNumber),
-                fun_fact = await _numberService.GetFunFact(parsedNumber)
+                is_Prime = IsPrime(parsedNumber),
+                is_Perfect = IsPerfect(parsedNumber),
+                digit_sum= DigitSum(parsedNumber),
+                fun_fact = await GetFunFact(parsedNumber)
             };
 
             // Determine properties based on the new requirements
-            bool isArmstrong = _numberService.IsArmstrong(parsedNumber);
+            bool isArmstrong = IsArmstrong(parsedNumber);
             bool isOdd = parsedNumber % 2 != 0;
 
             if (isArmstrong && isOdd)
